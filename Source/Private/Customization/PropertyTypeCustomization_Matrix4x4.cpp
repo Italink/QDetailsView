b6d1a915bba74bae4c6a31d2a33ec38a15371d45
italink <657959053@qq.com>
2025-08-25 15:02:45 +0800

    [feature] 增加矩阵扩展

--- Source/Private/Customization/PropertyTypeCustomization_Matrix4x4.cpp
+++ Source/Private/Customization/PropertyTypeCustomization_Matrix4x4.cpp
@@ -2,43 +2,93 @@
 #include "QQuickDetailsViewLayoutBuilder.h"
 #include "QPropertyHandle.h"
 #include <QMetaType>
+#include <QObject>
+#include <QMatrix4x4>
+#include "QQuickFunctionLibrary.h"
 
 void PropertyTypeCustomization_Matrix4x4::customizeChildren(QPropertyHandle* inPropertyHandle, QQuickDetailsViewLayoutBuilder* inBuilder)
 {
-	QPropertyHandle* position = QPropertyHandle::FindOrCreate(
-		inPropertyHandle->parent(),
-		QMetaType::fromType<QVector3D>(),
-		inPropertyHandle->createSubPath("Position"),
-		[inPropertyHandle]() {
-			return QVector3D();
-		},
-		[inPropertyHandle](QVariant var) {
-		}
-	);
-	inBuilder->addProperty(position);
+	//inBuilder->addCustomRow([inPropertyHandle](QQuickDetailsViewRowBuilder* builder) {
+	//	auto editorSlot = builder->makeNameValueSlot();
+	//	builder->setupLabel(editorSlot.first, "0");
+	//	auto valueItem = builder->setupItem(editorSlot.second, R"(
+	//				import QtQuick;
+	//				import QtQuick.Controls;
+	//				import "qrc:/Resources/Qml/ValueEditor"
+	//				Vec4Box{
+	//					anchors.verticalCenter: parent.verticalCenter
+	//					width: parent.width
+	//				}
+	//			)");
+	//	builder->setHeightProxy(valueItem);
+	//	QMatrix4x4 mat = inPropertyHandle->getVar().value<QMatrix4x4>();
+	//	valueItem->setProperty("value", mat.row(0));
 
-	QPropertyHandle* rotation = QPropertyHandle::FindOrCreate(
-		inPropertyHandle->parent(),
-		QMetaType::fromType<QVector3D>(),
-		inPropertyHandle->createSubPath("Rotation"),
-		[inPropertyHandle]() {
-			return QVector3D();
-		},
-		[inPropertyHandle](QVariant var) {
-		}
-	);
-	inBuilder->addProperty(rotation);
+	//	QQuickFunctionLibrary::connect(valueItem, SIGNAL(asValueChanged(QVariant)), inPropertyHandle, [inPropertyHandle](QVariant var) {
+	//		QMatrix4x4 mat = inPropertyHandle->getVar().value<QMatrix4x4>();
+	//		mat.setRow(0, var.value<QVector4D>());
+	//		inPropertyHandle->setVar(mat);
+	//	});
+	//	QQuickFunctionLibrary::connect(inPropertyHandle, SIGNAL(asRequestRollback(QVariant)), valueItem, [inPropertyHandle, valueItem](QVariant var) {
+	//		QMatrix4x4 mat = var.value<QMatrix4x4>();
+	//		valueItem->setProperty("value", mat.row(0));
+	//	});
+	//});
 
-	QPropertyHandle* scale = QPropertyHandle::FindOrCreate(
+	inBuilder->addProperty(QPropertyHandle::FindOrCreate(
 		inPropertyHandle->parent(),
-		QMetaType::fromType<QVector3D>(),
-		inPropertyHandle->createSubPath("Scale"),
+		QMetaType::fromType<QVector4D>(),
+		inPropertyHandle->createSubPath("Row 0"),
 		[inPropertyHandle]() {
-			return QVector3D();
+			return inPropertyHandle->getVar().value<QMatrix4x4>().row(0);
 		},
 		[inPropertyHandle](QVariant var) {
+			QMatrix4x4 mat = inPropertyHandle->getVar().value<QMatrix4x4>();
+			mat.setRow(0, var.value<QVector4D>());
+			inPropertyHandle->setVar(mat);
 		}
-	);
-	inBuilder->addProperty(scale);
+	));
+
+	inBuilder->addProperty(QPropertyHandle::FindOrCreate(
+		inPropertyHandle->parent(),
+		QMetaType::fromType<QVector4D>(),
+		inPropertyHandle->createSubPath("Row 1"),
+		[inPropertyHandle]() {
+			return inPropertyHandle->getVar().value<QMatrix4x4>().row(1);
+		},
+		[inPropertyHandle](QVariant var) {
+			QMatrix4x4 mat = inPropertyHandle->getVar().value<QMatrix4x4>();
+			mat.setRow(1, var.value<QVector4D>());
+			inPropertyHandle->setVar(mat);
+		}
+	));
+
+	inBuilder->addProperty(QPropertyHandle::FindOrCreate(
+		inPropertyHandle->parent(),
+		QMetaType::fromType<QVector4D>(),
+		inPropertyHandle->createSubPath("Row 2"),
+		[inPropertyHandle]() {
+			return inPropertyHandle->getVar().value<QMatrix4x4>().row(2);
+		},
+		[inPropertyHandle](QVariant var) {
+			QMatrix4x4 mat = inPropertyHandle->getVar().value<QMatrix4x4>();
+			mat.setRow(2, var.value<QVector4D>());
+			inPropertyHandle->setVar(mat);
+		}
+	));
+
+	inBuilder->addProperty(QPropertyHandle::FindOrCreate(
+		inPropertyHandle->parent(),
+		QMetaType::fromType<QVector4D>(),
+		inPropertyHandle->createSubPath("Row 3"),
+		[inPropertyHandle]() {
+			return inPropertyHandle->getVar().value<QMatrix4x4>().row(3);
+		},
+		[inPropertyHandle](QVariant var) {
+			QMatrix4x4 mat = inPropertyHandle->getVar().value<QMatrix4x4>();
+			mat.setRow(3, var.value<QVector4D>());
+			inPropertyHandle->setVar(mat);
+		}
+	));
 }
 
